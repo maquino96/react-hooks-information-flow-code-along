5e109a85cc60dcde72cab963ca4fd1d3f7bc8024
maquino96 <matthewaquino96@gmail.com>
2021-03-17 16:14:03 -0400

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -6,10 +6,23 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  const [childColor, setChildColor] = useState('#FFF')
+
+  function handleChildClick (event) {
+    const newRandomColor = getRandomColor()
+    setColor(newRandomColor)
+    
+    const randomChildColor = getRandomColor()
+    setChildColor(randomChildColor)
+
+  
+    // console.log(randomColor)
+  }
+
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onClickHandler={handleChildClick} color={childColor}/>
+      <Child onClickHandler={handleChildClick} color={childColor}/>
     </div>
   );
 }
